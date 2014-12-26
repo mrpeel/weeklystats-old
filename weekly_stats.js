7869f05c372608500fea45deac34504b41e17845
Neil Kloot <mrpeel@gmail.com>
2014-12-26 11:37:56 +1100

    Monthly content usage in progress

--- weekly_stats.js
+++ weekly_stats.js
@@ -261,6 +261,8 @@
    */
   function renderWeekContentUsageChart(ids, startDate, endDate) {
 
+    var topPageNames = [];
+
     query({
       'ids': ids,
       'dimensions': 'ga:pageTitle',
@@ -269,7 +271,7 @@
       'start-date': startDate,
       'end-date':  endDate,
       'sort': '-ga:pageviews',
-      'max-results': 10
+      'max-results': 6
 
     })
     .then(function(response) {
@@ -277,7 +279,7 @@
       var data = [];
       var labels = [];
       var datasets = [];
-      var colors = ['#4D5360','#949FB1','#D4CCC5','#ADD7FE','#FEADAD','#C1FEC2', '#C1FEE3', '#FFF9D1', '#F1FFD1', '#D1FFD1', '#D1FFEA'];
+      var colors = ['#4D5360','#949FB1','#D4CCC5','#ADD7FE','#FEADAD','#C1FEC2'];
 
       response.rows.forEach(function(row, i) {
         //labels.push(row[0]);
@@ -287,7 +289,8 @@
                         fillColor: colors[i],
                         strokeColor: colors[i],
                         data: [+row[1]]
-                      }  );
+                      } );
+        topPageNames.push(row[0]);
       });
 
       data = {labels: labels, datasets: datasets};
@@ -295,7 +298,7 @@
       new Chart(makeCanvas('weekly-content-chart-container')).Bar(data, {barDatasetSpacing : 10});
       generateLegend('weekly-content-legend-container', data.datasets);
 
-      _.delay(renderMonthlyContentUsageChart, 500 + Math.random()*500,ids, endDate);
+      _.delay(renderMonthlyContentUsageChart, 500 + Math.random()*500, ids, endDate, topPageNames);
 
     })
     .catch(function(err) {
@@ -309,33 +312,30 @@
    * Draw the a chart.js doughnut chart with data from the specified view that
    * shows the content for the week.
    */
-  function renderMonthlyContentUsageChart(ids, endDate) {
+  function renderMonthlyContentUsageChart(ids, endDate, topPages) {
 
-  /* Retrieve top 6 pages for last month and use values to create 6 copairson queries
+  /* Use top 4 pages for last week and run queries for
+  * 1st day of 12 months ago - last day of 10 months ago
+  * 1st day of 9 months ago - last day of 7 months ago
+  * 1st day of 6 months ago - last day of 4 months ago
+  * 1st day of 3 months ago - last day of last month
   */
 
-  var topPages =[];
+  //build month labels for 6 time periods
+  var monthLabels =[];
 
-    query({
-      'ids': ids,
-      'dimensions': 'ga:pageTitle',
-      'metrics': 'ga:pageviews',
-      'filters': 'ga:pageTitle!=Redirect;ga:pageviews>10',
-      'start-date': moment(endDate).subtract(1, 'months').format('YYYY-MM-DD'),
-      'end-date':  endDate,
-      'sort': '-ga:pageviews',
-      'max-results': 6
+  for (var i = 3; i >=0; i++) {
+    monthLabels.push(moment(endDate)subtract((i*3) + 3, 'months').date(1).format('MMM')
+      + '/' + moment(endDate)subtract((i*3) + 1, 'months').date(1).format('MMM'));
+  }
 
-    })
-    .then(function(response) {
 
-      response.rows.forEach(function(row, i) {
-        topPages.push(row[0]);
-      });
-    })
-    .catch(function(err) {
-      console.error(err.error.message);
-    })
+
+  var firstPeriodResults =[];
+  var secondPeriodResults =[];
+  var thirdPeriodResults =[];
+  var fourthPeriodResults =[];
+
 
     query({
       'ids': ids,
@@ -368,8 +368,33 @@
 
       data = {labels: labels, datasets: datasets};
 
-      new Chart(makeCanvas('monthly-content-chart-container')).Bar(data, {barDatasetSpacing : 10});
+      var data = {
+        labels : monthLabels,
+        datasets : [
+          {
+            //Set week ending date
+            //label: 'Last Week',
+            label: 'Week Starting ' + moment(startDate).format('DD/MM/YYYY'),
+            fillColor : "rgba(220,220,220,0.5)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : data2
+          },
+          {
+            label: 'Week Starting ' + moment(startDate).subtract(7, 'days').format('DD/MM/YYYY'),
+            fillColor : "rgba(151,187,205,0.5)",
+            strokeColor : "rgba(151,187,205,1)",
+            pointColor : "rgba(151,187,205,1)",
+            pointStrokeColor : "#fff",
+            data : data1
+          }
+        ]
+      };
+
+      new Chart(makeCanvas('monthly-content-chart-container')).Line(data);
       generateLegend('monthly-content-legend-container', data.datasets);
+
     })
     .catch(function(err) {
       console.error(err.error.message);
